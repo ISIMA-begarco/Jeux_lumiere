3019b103832e5badaa983919bb800d4b354e9b53
BBS007 <bbs07@laposte.net>
2014-01-23 18:47:41 +0100

    First commit of c file

---
+++ Programme/Jeux_lumiere.c
@@ -0,0 +1,94 @@
+void main() {
+
+///Initialisation des LED à l'etat eteint
+    PORTA = 0;
+    PORTB = 0;
+    PORTC = 0;
+    PORTD = 0;
+    PORTE = 0;
+    
+///
+    TRISB = 0;
+    
+    
+}
+
+void LED_zero()
+{
+ PORTA = 0xFF;
+ PORTB = 0b10000001;
+ PORTC = 0b10000001;
+ PORTD = 0xFF;
+}
+
+void LED_un()
+{
+ PORTA = 0b1000;
+ PORTB = 0b100;
+ PORTC = 0b10;
+ PORTD = 0xFF;
+}
+
+void LED_deux()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_trois()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_quatre()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_cinq()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_six()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_sept()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_huit()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
+
+void LED_neuf()
+{
+ PORTA =
+ PORTB =
+ PORTC =
+ PORTD =
+}
