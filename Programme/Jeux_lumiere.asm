ab0e39b0e794a2bac574239dcbb39d3d4d419422
Benoît Garçon <benoit.garconbesson@gmail.com>
2014-01-26 23:02:33 +0100

    tentative de game of life
    problèmes de mémoire

--- Programme/Jeux_lumiere.asm
+++ Programme/Jeux_lumiere.asm
@@ -1,14 +1,44 @@
 
 _main:
 
-;Jeux_lumiere.c,34 :: 		void main()
-;Jeux_lumiere.c,37 :: 		print_logo_ISIMA();
-	CALL        _print_logo_ISIMA+0, 0
-;Jeux_lumiere.c,40 :: 		while(1)
+;Jeux_lumiere.c,44 :: 		void main()
+;Jeux_lumiere.c,48 :: 		while(1)
 L_main0:
-;Jeux_lumiere.c,43 :: 		}
+;Jeux_lumiere.c,59 :: 		Glcd_Init();
+	CALL        _Glcd_Init+0, 0
+;Jeux_lumiere.c,60 :: 		Glcd_Image(image_ISIMA);
+	MOVLW       _image_isima+0
+	MOVWF       FARG_Glcd_Image_image+0 
+	MOVLW       hi_addr(_image_isima+0)
+	MOVWF       FARG_Glcd_Image_image+1 
+	MOVLW       higher_addr(_image_isima+0)
+	MOVWF       FARG_Glcd_Image_image+2 
+	CALL        _Glcd_Image+0, 0
+;Jeux_lumiere.c,61 :: 		Delay_ms(5000);
+	MOVLW       51
+	MOVWF       R11, 0
+	MOVLW       187
+	MOVWF       R12, 0
+	MOVLW       223
+	MOVWF       R13, 0
+L_main2:
+	DECFSZ      R13, 1, 1
+	BRA         L_main2
+	DECFSZ      R12, 1, 1
+	BRA         L_main2
+	DECFSZ      R11, 1, 1
+	BRA         L_main2
+	NOP
+	NOP
+;Jeux_lumiere.c,75 :: 		LED_init();
+	CALL        _LED_init+0, 0
+;Jeux_lumiere.c,77 :: 		decompte();
+	CALL        _decompte+0, 0
+;Jeux_lumiere.c,78 :: 		LED_init();
+	CALL        _LED_init+0, 0
+;Jeux_lumiere.c,79 :: 		}
 	GOTO        L_main0
-;Jeux_lumiere.c,44 :: 		}
+;Jeux_lumiere.c,80 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- Programme/glcd.h
+++ Programme/glcd.h
@@ -1,6 +1,13 @@
 #ifndef GLCD_H_INCLUDED
 #define GLCD_H_INCLUDED
 
-void print_logo_ISIMA();
+#define SCREEN_W       128
+#define SCREEN_H       64
+
+void game_of_life(unsigned char ** p_monTab); /// effectue une étape du célèbre jeu de la vie
+
+void image_to_matrix(const char * entree, unsigned char ** retour); /// convertit les images standart en image pour nous
+void matrix_to_image(unsigned char ** entree, char retour[1024]); /// inversement
+void print_glcd(unsigned char ** image); /// imprime nos images
 
 #endif // GLCD_H_INCLUDED
